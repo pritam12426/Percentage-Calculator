3c0c2e659c63d093c5418d3be0072aed2ef75078
Pritam <84720825+pritam12426@users.noreply.github.com>
2023-02-03 20:59:10 +0530

    Some comments has been added for Porgrammer:
    * COMMENTS
    * SOME LINE SPELLING MISTAKES

--- Main.py
+++ Main.py
@@ -1,32 +1,33 @@
-import os # Os is bulit in module
-from playsound import playsound # Pip install playsound
-from plyer import notification # Pip install plyer
+import os  # Os is bulit in module
+from playsound import playsound  # Pip install playsound
+from plyer import notification  # Pip install plyer
 
-if(not os.path.exists("Output")):
+if (not os.path.exists("Output")):
     os.mkdir("Output")
-while True:
+
+while (True): # This will take input for maximum of a single subject
     try:
         max_marks = int(input("Inset the maximum of a single subject >  "))
         break
     except:
         print("\tError: Maximum single subject makrs must be number")
-        playsound('Porgram_Data/Error.wav') 
+        playsound('Porgram_Data/Error.wav')
 
-while(True):
+while (True): # This will take input for class 1 to 12
     try:
         Class = int(input("Inset only Class name in numbers >  "))
-        if 1<=Class and 12>=Class:
+        if 1 <= Class and 12 >= Class:
             break
         else:
             print("\tError: The Class must lise between 1 and 12.")
-            playsound('Porgram_Data/Error.wav') 
+            playsound('Porgram_Data/Error.wav')
     except:
         playsound('Porgram_Data/Error.wav')
         print("\tError: Class must be a number")
 
-while True:
+while (True): # Will take input for sec of class
     Class_section = input(f"Inset the the section fo class > ")
-    if Class_section.isalpha():
+    if Class_section.isalpha(): # Will check the Class_section is not carring any "alphabetic characters"
         if len(Class_section) == 1:
             break
         else:
@@ -38,9 +39,9 @@ while True:
 
 file_name = "Class_"
 
-if(not os.path.exists(f'Output/{file_name}{Class}{Class_section.title()}.csv')):
+if (not os.path.exists(f'Output/{file_name}{Class}{Class_section.title()}.csv')): # Will add heading for file. If it is not availabe is the output folder
 
-    mylist = ['Name','English','Maths','Hindi','Sst','Science','Total Marks','Percentage']
+    mylist = ['Name', 'English', 'Maths', 'Hindi','Sst', 'Science', 'Total Marks', 'Percentage']
     with open(f'Output/{file_name}{Class}{Class_section.title()}.csv', 'w') as f:
         for hedder in mylist:
             f.write(str(hedder))
@@ -48,32 +49,34 @@ if(not os.path.exists(f'Output/{file_name}{Class}{Class_section.title()}.csv')):
                 f.write(str(','))
 
 h = 0
-while True:
+while (True):
     student_name = input("Insert the name of sudent or enter '-' to close > ")
     student_name = student_name.title()
     if student_name.find("-") == 0:
-        if not h==0:
+        if not h == 0:
             notification.notify(
-            title = "Percentage calculator",
-            message = f"A file of name {Class}_{Class_section.upper()} is save in Output folder.",
-            app_icon = "Porgram_Data/icon.ico",
-            timeout = 3)
+                title="Percentage calculator",
+                message=f"A file of name {Class}_{Class_section.upper()} is save in Output folder.",
+                app_icon="Porgram_Data/icon.ico",
+                timeout=3)
         break
     else:
-        with open(f'Output/{file_name}{Class}{Class_section.title()}.csv','a') as file_write:
+        with open(f'Output/{file_name}{Class}{Class_section.title()}.csv', 'a') as file_write:
             file_write.write(f"\n{str(student_name)},")
 
             total_marks = 0
-            subject_list = ['English','Maths','Hindi','Sst','Science']
+            subject_list = ['English', 'Maths', 'Hindi', 'Sst', 'Science'] # Will help to user to input the date
 
             for subject in subject_list:
                 while True:
                     try:
-                        marks = float(input(f"Inset the mark of {student_name} in subject {subject} > "))
-                        if 0<=marks and max_marks>=marks:
+                        marks = float(
+                            input(f"Inset the mark of {student_name} in subject {subject} > "))
+                        if 0 <= marks and max_marks >= marks:
                             break
                         else:
-                            print(f"\tError: The marks must lise between 0 and {max_marks}.")
+                            print(
+                                f"\tError: The marks must lise between 0 and {max_marks}.")
                             playsound('Porgram_Data/Error.wav')
                     except ValueError:
                         print("\tError: The Insert Value must be a number.")
@@ -83,17 +86,20 @@ while True:
 
             a = max_marks*len(subject_list)
             percentage = (total_marks / a) * 100
-            percentage = float('%.2f'% percentage)
-            file_write.write(str(f"{total_marks}/{max_marks*len(subject_list)},"))
+            percentage = float('%.2f' % percentage)
+
+            file_write.write(
+                str(f"{total_marks}/{max_marks*len(subject_list)},"))
+
             file_write.write(str(f"{percentage}%,"))
             playsound('Porgram_Data/pop.wav')
-            h +=1
+            h += 1
 
-        if percentage>=60:
+        if percentage >= 60:
             print(f"\t{student_name} got {percentage}% and passed with first divison.\n")
-        elif percentage>=40:
+        elif percentage >= 40:
             print(f"\t{student_name} got {percentage}% and passed with secound divison.\n")
-        elif percentage>=33:
+        elif percentage >= 33:
             print(f"\t{student_name} got {percentage}% and passed with third divison.\n")
         else:
             print(f"\t{student_name} got {percentage}% and {student_name} is fail\n")
